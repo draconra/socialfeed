f4d53fb38c730406cf79e186bff1aca661998f86
Aditya Sumardi <aditya.sumardi@dana.id>
2021-09-07 22:49:01 +0700

    Add item list feed layout

--- app/src/main/java/com/aditya/socialfeed/FeedFragment.kt
+++ app/src/main/java/com/aditya/socialfeed/FeedFragment.kt
@@ -1,19 +1,18 @@
 package com.aditya.socialfeed
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.aditya.socialfeed.databinding.FragmentFirstBinding
+import androidx.fragment.app.Fragment
+import com.aditya.socialfeed.databinding.FragmentFeedBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FeedFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentFeedBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,17 +23,13 @@ class FirstFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentFeedBinding.inflate(inflater, container, false)
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 
     override fun onDestroyView() {
